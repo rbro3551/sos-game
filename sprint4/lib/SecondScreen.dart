d33dd1bf340694bb82756fe5668ea991116cd9c9
rbro3551 <rileybrookins2@gmail.com>
2022-11-15 19:58:19 -0600

    added sprint 4

---
+++ sprint4/lib/SecondScreen.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:tic_tac_toe_example/MainScreen.dart';
+
+class SecondScreen extends StatefulWidget {
+  final String title;
+  final int value;
+  final String modeval;
+
+  SecondScreen(
+      {Key? key,
+      required this.title,
+      required this.value,
+      required this.modeval})
+      : super(key: key);
+
+  @override
+  State<SecondScreen> createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  static String bluebot = "blue bot?";
+  static String redbot = "red bot?";
+  Map<String, bool> bots = {
+    redbot: false,
+    bluebot: false,
+  };
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Game Mode Selection'),
+        automaticallyImplyLeading: false,
+      ),
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          ElevatedButton(
+              key: ValueKey('simplebutton'),
+              onPressed: () {
+                String modeval = "Simple Game";
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => MainPage(
+                        title: modeval,
+                        value: widget.value,
+                        redbot: bots[redbot],
+                        bluebot: bots[bluebot])));
+              },
+              child: const Text('Simple Game')),
+          SizedBox(height: 30),
+          ElevatedButton(
+            key: ValueKey('genbutton'),
+            onPressed: () {
+              String modeval = "General Game";
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => MainPage(
+                      title: modeval,
+                      value: widget.value,
+                      redbot: bots[redbot],
+                      bluebot: bots[bluebot])));
+            },
+            child: const Text('General Game'),
+          ),
+          checkbox(bluebot, bots[bluebot]!),
+          checkbox(redbot, bots[redbot]!)
+        ]),
+      ),
+    );
+  }
+
+  Widget checkbox(String title, bool boolValue) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(title),
+        Checkbox(
+          value: boolValue,
+          onChanged: (value) => setState(() => bots[title] = value!),
+        )
+      ],
+    );
+  }
+}
